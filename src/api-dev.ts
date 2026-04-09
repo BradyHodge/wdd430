d63f1bcfc0af42b1e2809c3c6b53889d03add36d
Brady Hodge <bhodge@nvidia.com>
2026-04-08 22:20:49 -0600

    fix tsx type mod

--- src/api-dev.ts
+++ src/api-dev.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { connectMongo } from './server/db';
 import { registerApi } from './server/register-api';
 
-const app = express();
-registerApi(app);
-await connectMongo();
+(async () => {
+  const app = express();
+  registerApi(app);
+  await connectMongo();
 
-const port = Number(process.env['API_PORT'] ?? 3000);
-app.listen(port, () => {
-  console.log(`REST API at http://localhost:${port} (MongoDB: ${process.env['MONGODB_URI'] ?? 'mongodb://127.0.0.1:27017/wdd430'})`);
-});
+  const port = Number(process.env['API_PORT'] ?? 3000);
+  app.listen(port, () => {
+    console.log(`REST API at http://localhost:${port} (MongoDB: ${process.env['MONGODB_URI'] ?? 'mongodb://127.0.0.1:27017/wdd430'})`);
+  });
+})();
